13273a1dc27bb4c2bf2e55e10c6cd9737a9cb43c
77-223255 <77-223255@users.noreply.github.com>
2026-04-17 21:55:34 +0800

    feat: add pretrain script and training data

--- llmbuild/4_pretrain/GPT_main_structure_pack.py
+++ llmbuild/4_pretrain/GPT_main_structure_pack.py
@@ -4,18 +4,6 @@ import tiktoken
 import matplotlib.pyplot as plt
 from self_attention_pack import MultiHeadAttention
 
-GPT_CONFIG_124M = {
-    "vocab_size": 50257,
-    "context_length": 1024,
-    "emb_dim": 768,
-    "n_heads": 12,
-    "n_layers": 12,
-    "drop_rate": 0.1,
-    "qkv_bias": False
-}
-
-
-
 class LayerNorm(nn.Module):
     def __init__(self, emb_dim):
         super().__init__()
